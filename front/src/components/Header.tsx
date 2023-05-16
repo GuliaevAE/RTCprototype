a527e0cad2bfb63211c19e83ee9fcf7c19b78c59
Anton <antybumer@gmail.com>
2023-05-16 22:47:26 +0300

    some smol fix

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -1,10 +1,11 @@
 import axios from 'axios';
 import React from 'react';
-import { useAppDispatch } from '../store/hooks'
-import { changeOriginalStaf } from '../store/slices/goodsSlice';
+import { useAppDispatch, useAppSelector } from '../store/hooks'
+import { OriginalStaf, changeOriginalStaf } from '../store/slices/goodsSlice';
 
 const Header = () => {
     const dispatch = useAppDispatch()
+    const originalStaf = useAppSelector(OriginalStaf)
 
     const submitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
@@ -21,6 +22,7 @@ const Header = () => {
             const ind = String(i).length === 1 ? '0' + i : i
             try {
                 const res = await axios.get(`https://basket-${ind}.wb.ru/vol${vol}/part${part}/${all}/info/ru/card.json`)
+                res.data.links = { link_wb: `https://www.wildberries.ru/catalog/${all}/detail.aspx`, link_mp: `https://mpstats.io/wb/item/${all}` }
                 return dispatch(changeOriginalStaf(res.data))
             } catch (error) {
                 console.log(error)
@@ -29,12 +31,21 @@ const Header = () => {
     }
 
     return (
-        <header className='w-full pt-[3vmin] px-[5vmin] flex relative z-[1]'>
+        <header className='w-full pt-[3vmin] px-[5vmin] flex relative z-[1] gap-4'>
             <form onSubmit={event => submitHandler(event)} className='flex'>
-                <input id='originalStaf_input' className='border-[.1px] border-solid border-[white] text-[white] bg-[black] px-2 rounded-l' placeholder='Идентификатор WB'  type="text" />
+                <input id='originalStaf_input' className='border-[.1px] border-solid border-[white] text-[white] bg-[black] px-2 rounded-l' placeholder='Идентификатор WB' type="text" />
                 <button className='border-[.1px] border-solid border-[white] rounded-r p-1 px-2 '>Найти</button>
             </form>
+            {originalStaf && <div className='header_links flex gap-[2vmin]'>
+                <div className=''>
+                    <a target="_blank" href={originalStaf.links.link_wb} className='leading-[2rem]'>WB</a>
+                </div>
+                <div className=''>
+                    <a target="_blank" href={originalStaf.links.link_mp} className='leading-[2rem]'>MP</a>
+                </div>
 
+            </div>
+            }
         </header>
     );
 };
