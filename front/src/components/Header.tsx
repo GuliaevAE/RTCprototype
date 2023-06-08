5c5f37502a55ee2fbb279cbf2bd17f5cb8cdeb79
Anton <antybumer@gmail.com>
2023-06-08 22:12:31 +0300

    header style fix

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -9,10 +9,10 @@ const Header = () => {
 
     return (
         <div className='flex gap-2 text-[1.1rem] font-[600] fixed z-[99] left-[0] px-[5vmin] top-[4vmin] w-full '>
-            <Link to={'/catalog'}><span className={`px-2 py-1 border-solid border-[black] border-[2px] rounded-[10px] ${loc.pathname === '/catalog' ? 'bg-[black] text-[white]' : 'bg-[white] text-[black]'} `}>Каталог</span></Link>
-            <Link to={'/'}><span className={`px-2 py-1 border-solid border-[black] border-[2px] rounded-[10px] ${loc.pathname === '/' ? 'bg-[black] text-[white]' : 'bg-[white] text-[black]'} `}>Калькулятор</span></Link>
+            <Link to={'/catalog'}><span className={`px-2 py-1 border-solid border-[black] border-[2px] rounded-[5px] ${loc.pathname === '/catalog' ? 'bg-[black] text-[white]' : 'bg-[white] text-[black]'} `}>Каталог</span></Link>
+            <Link to={'/'}><span className={`px-2 py-1 border-solid border-[black] border-[2px] rounded-[5px] ${loc.pathname === '/' ? 'bg-[black] text-[white]' : 'bg-[white] text-[black]'} `}>Калькулятор</span></Link>
             <div className=''>
-                <span onClick={() => dispatch(changeAnimSwitcher())} className={`${animSwitcher ? 'bg-[black] text-[white]' : 'bg-[white] text-[black]'} border-solid border-[2px] border-[black] px-2 py-1  rounded-[10px]`}>{animSwitcher ? 'ON' : 'OFF'}</span>
+                <span onClick={() => dispatch(changeAnimSwitcher())} className={`${animSwitcher ? 'bg-[black] text-[white]' : 'bg-[white] text-[black]'} border-solid border-[2px] border-[black] px-2 py-1  rounded-[5px]`}>{animSwitcher ? 'ON' : 'OFF'}</span>
             </div>
         </div>
     );
